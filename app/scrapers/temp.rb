3e6a0c7a3107961c119503b0bad41e8f52314b80
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2016-12-23 17:45:22 -0500

    enabled adding professors and courses to db

--- app/scrapers/temp.rb
+++ app/scrapers/temp.rb
@@ -15,8 +15,8 @@ db_test = MongoClient.new(host, port, pool_size: 2, pool_timeout: 2).db('testing
 
 # Architecture:
 # build list of queries
-semesters_to_check = ["courses201601", "courses201605", "courses201608", "courses201612"]
-course_collections = semesters_to_check.map { |e| db_class.collection(e) }
+semesters_to_check = ["201601"]
+course_collections = semesters_to_check.map { |e| db_class.collection("courses#{e}") }
 section_queries = []
 course_collections.each do |c|
 	semester = c.name.scan(/courses(.+)/)[0]
@@ -35,9 +35,10 @@ puts "added all urls"
 # store in a hash of course id => array of sections?
 # for now, just store array of course ids
 
+prof_coll = db_test.collection("testing_profs")
+
 section_queries.each do |query|
 	page = Nokogiri::HTML(open(query))
-	prof_coll = db_test.collection("testing")
 	bulk = prof_coll.initialize_unordered_bulk_op
 
 
@@ -50,13 +51,26 @@ section_queries.each do |query|
 	course_divs.each do |course_div|
 		course_id = course_div.attr('id')
 
+		puts "Getting prof for #{course_id} (#{semester})"
+
 		course_div.search('div.section').each do |section|
 			instructors = section.search('span.section-instructors').text.gsub(/\t|\r\n/,'').encode('UTF-8', :invalid => :replace).split(',').map(&:strip)
-			puts "#{course_id}: #{instructors}"
+			instructors.each do |x| 
+				profs[x] ||= []
+				profs[x] |= [course_id]
+			end
 		end
 	end
 
-
+	profs.sort.to_h.each do |name, courses|
+		# push all courses to prof's entry
+		bulk.find({name: name}).upsert.update(
+			{"$set" => {name: name},
+			 "$addToSet" => {courses: {"$each" => courses} }
+			}
+		)
+	end
+	bulk.execute unless profs.empty?
 end
 
 
